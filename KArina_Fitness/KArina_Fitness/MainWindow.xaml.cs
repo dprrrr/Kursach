1930504429995c55c7313736aae7ee2941f5f67d
VeronikaToporova <veronichka.toporova@list.ru>
2021-06-04 13:42:02 +0300

    Приложение добавила

---
+++ KArina_Fitness/KArina_Fitness/MainWindow.xaml.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KArina_Fitness
+{
+
+    public partial class ServiceKarina
+    {
+        // ссылка на картинку
+        // по ТЗ, если картинка не найдена, то должна выводиться картинка по-умолчанию
+        // в XAML-е можно это сделать средствами разметки, но там есть условие что вместо ссылки на картинку получен NULL
+        // у нас же возможна ситуация, когда в базе есть путь к картинке, но самой картинки в каталоге нет
+        // поэтому я сделал проверку наличия файла картинки и возвращаю картинку по-умолчанию, если нужной нет 
+        public Uri ImagePreview
+        {
+            get
+            {
+                var imageName = System.IO.Path.Combine(Environment.CurrentDirectory, photo ?? "");
+                return System.IO.File.Exists(imageName) ? new Uri(imageName) : new Uri("pack://application:,,,/Images/picture.png");
+            }
+        }
+
+
+
+        public Boolean MaxBalance
+        {
+            get
+            {
+                return Price < 3000;
+            }
+        }
+
+        public string BalanceString
+        {
+            get
+            {
+                // Convert.ToDecimal - преобразует double в decimal
+                // Discount ?? 0 - разнуливает "Nullable" переменную
+                return Price.ToString("#.##");
+            }
+        }
+
+        public float PriceFloat
+        {
+            get
+            {
+                return Convert.ToSingle(Price);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private List<ServiceKarina> _ServiceList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<ServiceKarina> ServiceList
+        {
+            get
+            {
+                var FilteredServiceList = _ServiceList.FindAll(item =>
+                item.PriceFloat >= CurrentDiscountFilter.Item1 &&
+                item.PriceFloat < CurrentDiscountFilter.Item2);
+
+                if (SearchFilter != "")
+                    FilteredServiceList = FilteredServiceList.Where(item =>
+                        item.Title.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1 ||
+                        item.BalanceString.IndexOf(SearchFilter, StringComparison.OrdinalIgnoreCase) != -1).ToList();
+
+                if (SortPriceAscending)
+                    return FilteredServiceList
+                    .OrderBy(item => Double.Parse(item.BalanceString))
+                .ToList();
+                else
+                    return FilteredServiceList
+                        .OrderByDescending(item => Double.Parse(item.BalanceString))
+                        .ToList();
+            }
+            set
+            {
+                _ServiceList = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ProductCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredProductCount"));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ServiceList = Core.DB.ServiceKarina.ToList();
+        }
+
+        private Boolean _SortPriceAscending = true;
+        public Boolean SortPriceAscending
+        {
+            get { return _SortPriceAscending; }
+            set
+            {
+                _SortPriceAscending = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ProductCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredProductCount"));
+
+                }
+            }
+        }
+
+        private List<Tuple<string, float, float>> FilterByDiscountValuesList =
+        new List<Tuple<string, float, float>>() {
+        Tuple.Create("Все цены", 0f, 20000f),
+        Tuple.Create("от 0 до 1000", 0f, 1000f),
+        Tuple.Create("от 1000 до 2000", 1000f, 2000f),
+        Tuple.Create("от 2000 до 10000", 2000f, 10000f)
+        };
+
+        public List<string> FilterByDiscountNamesList
+        {
+            get
+            {
+                return FilterByDiscountValuesList
+                    .Select(item => item.Item1)
+                    .ToList();
+            }
+        }
+
+
+        private Tuple<float, float> _CurrentDiscountFilter = Tuple.Create(float.MinValue, float.MaxValue);
+
+        public Tuple<float, float> CurrentDiscountFilter
+        {
+            get
+            {
+                return _CurrentDiscountFilter;
+            }
+            set
+            {
+                _CurrentDiscountFilter = value;
+                if (PropertyChanged != null)
+                {
+                    // при изменении фильтра список перерисовывается
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ProductCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredProductCount"));
+
+                }
+            }
+        }
+
+        private void DiscountFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CurrentDiscountFilter = Tuple.Create(
+                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item2,
+                FilterByDiscountValuesList[DiscountFilterComboBox.SelectedIndex].Item3
+            );
+        }
+
+
+        private string _SearchFilter = "";
+        public string SearchFilter
+        {
+            get { return _SearchFilter; }
+            set
+            {
+                _SearchFilter = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("ServicesCount"));
+                    PropertyChanged(this, new PropertyChangedEventArgs("FilteredServiceCount"));
+                }
+            }
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            SearchFilter = SearchFilterTextBox.Text;
+        }
+
+
+        public int ProductCount
+        {
+            get
+            {
+                return _ServiceList.Count;
+            }
+
+        }
+        public int FilteredProductCount
+        {
+            get
+            {
+                return ServiceList.Count;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SortPriceAscending = (sender as RadioButton).Tag.ToString() == "1";
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Ord = new ServiceKarina();
+            var NewOrdWind = new CreateWindow(Ord);
+            if ((bool)NewOrdWind.ShowDialog())
+            {
+                ServiceList = Core.DB.ServiceKarina.ToList();
+                PropertyChanged(this, new PropertyChangedEventArgs("ServiceList"));
+                PropertyChanged(this, new PropertyChangedEventArgs("FilteredServiceCount"));
+                PropertyChanged(this, new PropertyChangedEventArgs("ServiceCount"));
+            }
+            //Ord.ShowDialog();
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void DelOrd_Click(object sender, RoutedEventArgs e)
+        {
+            var item = ProductListView.SelectedItem as ServiceKarina;
+            Core.DB.ServiceKarina.Remove(item);
+            Core.DB.SaveChanges();
+            ServiceList = Core.DB.ServiceKarina.ToList();
+        }
+
+
+        private void EditOrder_Click(object sender, RoutedEventArgs e)
+        {
+            var SelectedOrder = ProductListView.SelectedItem as ServiceKarina;
+            var EditOrderWindow = new CreateWindow(SelectedOrder);
+            if ((bool)EditOrderWindow.ShowDialog())
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("ServiceList")); ;
+            }
+
+        }
+
+    }
+}
